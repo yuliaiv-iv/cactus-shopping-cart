d41f5603e0b4d690fe73f7edf62c132268366e33
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2021-05-20 15:20:50 -0400

    fetch data from firebase

--- src/components/CactusesList/CactusesList.js
+++ src/components/CactusesList/CactusesList.js
@@ -1,16 +1,54 @@
-import { data } from '../../utils/data';
+// import { data } from '../../utils/data';
+import { useEffect, useState } from 'react';
 import CactusWrapper from '../CactusWrapper/CactusWrapper';
 import CactusCard from '../CactusCard/CactusCard';
 import './CactusesList.css';
 
 const CactusesList = () => {
-  const cactusList = data.map((c) => (
+
+  const [loadedData, setLoadedData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://shopping-cart-be698-default-rtdb.firebaseio.com/cactuses.json')
+    .then((res) => {
+      if (res.ok) {
+          return res.json()
+      }
+      return Promise.reject(`${res.status}`);
+    })
+    .then((data) => {
+      const fetchedData = [];
+      for (const key in data) {
+        fetchedData.push({
+          id: key,
+          name: data[key].name,
+          image_url: data[key].image_url,
+          price: data[key].price,
+        })
+      }
+      setLoadedData(fetchedData);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    })
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section>
+        <p className='cactuses__loading'>Loading...</p>
+      </section>
+    )
+  }
+
+  const cactusList = loadedData.map((c) => (
     <CactusCard
       key={c.id}
       id={c.id}
       name={c.name}
       image_url={c.image_url}
-      description={c.description}
       price={c.price}
     />
   ));
